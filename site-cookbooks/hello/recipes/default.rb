4ba1af82647183608e3402321a89291fb7f51db9
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2014-05-13 18:07:29 +0900

    init

---
+++ site-cookbooks/hello/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: hello
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+log "Hello, Chef!"
+
+package "git" do
+  action :install
+end
